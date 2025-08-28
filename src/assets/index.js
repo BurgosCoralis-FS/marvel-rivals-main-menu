54256bec9cad7932f3a8b5292a3cf4b25c3d960b
Cora <csburgosrodriguez@student.fullsail.edu>
2025-08-28 17:09:42 -0400

    updated imports to meet netlify needs

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,36 +1,36 @@
 //bg
-import marvelRivalsBg from './videos/mrMenu.mp4' 
-import homeBg from './images/home-bg.svg'
-import playBg from './images/play-btn.png'
-import imgBg from './images/img-bg.png'
+import marvelRivalsBg from '../videos/mrMenu.mp4' 
+import homeBg from '../images/home-bg.svg'
+import playBg from '../images/play-btn.png'
+import imgBg from '../images/img-bg.png'
 
 //logo
-import marvelLogo from './images/logo.ico'
-import mrLogo from './images/MRlogo.webp'
+import marvelLogo from '../images/logo.ico'
+import mrLogo from '../images/MRlogo.webp'
 
 //nav icons
-import blueCurrency from './images/blue-currency.svg'
-import goldCurrency from './images/gold-currency.svg'
-import settings from './images/settings.svg'
-import messages from './images/messages.svg'
-import friends from './images/friends.svg'
-import notification from './images/notification.svg'
-import profile from './images/profile.png'
+import blueCurrency from '../images/blue-currency.svg'
+import goldCurrency from '../images/gold-currency.svg'
+import settings from '../images/settings.svg'
+import messages from '../images/messages.svg'
+import friends from '../images/friends.svg'
+import notification from '../images/notification.svg'
+import profile from '../images/profile.png'
 
 //carousel images
-import mrVsMs from './images/mrxms.jpg'
-import bP from './images/battlepass.png'
+import mrVsMs from '../images/mrxms.jpg'
+import bP from '../images/battlepass.png'
 
 //event 
-import entangledMoments from './images/entangledMoments.png'
+import entangledMoments from '../images/entangledMoments.png'
 
 //challenges
-import challengesBg from './images/challenges.png'
-import calendar from './images/calendar.svg'
-import clock from './images/clock.svg'
+import challengesBg from '../images/challenges.png'
+import calendar from '../images/calendar.svg'
+import clock from '../images/clock.svg'
 
-import leftBtn from './images/leftBtn.svg'
-import chat from './images/chat.png'
+import leftBtn from '../images/leftBtn.svg'
+import chat from '../images/chat.png'
 
 export {
     marvelRivalsBg,
